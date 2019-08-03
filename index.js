2e979206fa64cfbc4e924f74a3ffb7090e63c40a
lesliesage <leslie.sage@gmail.com>
2019-08-03 12:40:20 -0400

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,10 @@
-// Write your code here!
+// no longer has DOM node 'main#main'
+document.querySelector('main#main').remove();
+
+// has a 'newHeader' variable that points to node 'h1#victory'
+let newHeader = document.createElement("h1");
+document.body.appendChild(newHeader);
+newHeader.setAttribute("id", "victory");
+
+// has a 'newHeader' variable that points to node 'h1#victory' with "YOUR-NAME is the champion" inside
+newHeader.innerHTML = "<h1>Leslie is the champion</h1>";
