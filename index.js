40d363186e40a1371c617f6790123c36ddb832e1
SkaMateria <skamateria@gmail.com>
2019-08-05 08:12:30 -0400

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,9 @@
-// Write your code here!
+var removeMain = document.getElementById('main');
+removeMain.remove();
+
+
+var newHeader = document.createElement('h1')
+newHeader.id = "victory"
+var champion = document.createTextNode(`YOUR-NAME is the champion`)
+newHeader.appendChild(champion)
+document.body.appendChild(newHeader)
